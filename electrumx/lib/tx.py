66a1146f35954eb80e73b8ca86f3cbc015efba6f
nkostoulas <nkostoulas@gmail.com>
2018-08-03 14:41:55 +0100

    Ocean Block, Block Header and Transaction deserialization models

--- electrumx/lib/tx.py
+++ electrumx/lib/tx.py
@@ -250,6 +250,273 @@ class DeserializerSegWit(Deserializer):
         tx, tx_hash, vsize = self._read_tx_parts()
         return tx, vsize
 
+class TxOcean(namedtuple("Tx", "version flag inputs outputs "
+                          "locktime inwitness outwitness")):
+    '''Class representing an Ocean transaction.'''
+    CONFIDENTIAL_COMMITMENT = 33    # default size of confidential commitments (i.e. asset, value, nonce)
+    CONFIDENTIAL_VALUE = 9          # explciti size of confidential values
+
+    @cachedproperty
+    def is_coinbase(self):
+        return (self.inputs[0].is_coinbase or
+                self.inputs[0].is_initial_issuance)
+
+class TxInputOcean(namedtuple("TxInput", "prev_hash prev_idx script sequence issuance")):
+    '''Class representing a transaction input.'''
+    ZERO = bytes(32)
+    MINUS_1 = 4294967295
+    OUTPOINT_ISSUANCE_FLAG = (1 << 31)
+    OUTPOINT_INDEX_MASK = 0x3fffffff
+
+    @cachedproperty
+    def is_coinbase(self):
+        return (self.prev_hash == TxInputOcean.ZERO and
+                self.prev_idx == TxInputOcean.MINUS_1)
+
+    ''' MAYBE not the best way of doing this
+    Initial issuance should not have a prev_hash but in ocean this is set to
+    a dummy commitment to the genesis arguments to be replaced by an actual
+    pegin bitcoin hash. Possible solution would be to hardcode prev_hash
+    Same treatment with coinbase transactions
+    '''
+    @cachedproperty
+    def is_initial_issuance(self):
+        return (self.is_issuance and
+                self.sequence == TxInputOcean.MINUS_1)
+
+    @cachedproperty
+    def is_issuance(self):
+        return (self.issuance is not None)
+
+    def __str__(self):
+        script = self.script.hex()
+        prev_hash = hash_to_hex_str(self.prev_hash)
+        return ("Input({}, {:d}, script={}, sequence={:d})"
+                .format(prev_hash, self.prev_idx, script, self.sequence))
+
+class TxOutputOcean(namedtuple("TxOutput", "asset value nonce pk_script")):
+    '''Class representing a transaction output.'''
+    pass
+
+class TxInputIssuanceOcean(namedtuple("TxInputIssuance", "nonce entropy amount inflation")):
+    '''Class representing a transaction input issuance.'''
+    pass
+
+class DeserializerOcean(Deserializer):
+    WITNESS_SCALE_FACTOR = 4;
+
+    '''
+    Ocean Block Header sample
+    00000020    version
+    f2f7342df785645fc5b28e4db3261eef0b4ef57ee787068374b103854cd65b08    prevhash
+    713ccf4863baa2d3cd2ed82b25e866dc660b0e1d9f359147fc968cd2ee074c99    hashMerkleRoot
+    5df6e0e2761359d30a8275058e299fcc0381534545f55cf43e41983f5d4c9456    hashContract
+    0000000000000000000000000000000000000000000000000000000000000000    hashAttestation
+    a4375f5b    nTime
+    01000000    nHeight
+    69          script challenge
+    522103d517f6e9affa60000a08d478970e6bbfa45d63b1967ed1e066dd46b802edb2a62102afc18e8a7ff988ca1ae7b659cb09a79852d301c2283e18cba1faf7a0b020b1a22102edd8080e31f05c68cf68a97782ac97744e86ba19dfd3ba68e597f10868ee5bc453ae
+    8e          script proof
+    0046304402201a822d9a7f211fbfbf2bb92ead874c71967dbd3c9e0249931cabb7591f36a46602207acbd97989005d0f16b6b6de84b03f3c659de28649a67cf2664e79badadf20c4453043021f5e1e160aa0e6afb078e9e2428d60a146598df03d48ede67799242109c2b690022023253a6381ea8cd07410903e97be730823a48a79bac16c4e097fe3fc54060888
+    '''
+    def read_header(self, height, static_header_size):
+        '''Return the Ocean block header bytes'''
+        start = self.cursor
+        self.cursor += static_header_size
+
+        challenge_size = self._read_varint() # read challenge size - 2 bytes
+        if challenge_size:
+            self.cursor += challenge_size # read challenge - challenge_size bytes
+
+        proof_size = self._read_varint() # read proof size - 2 bytes
+        if proof_size:
+            self.cursor += proof_size # read proof
+
+        header_end = self.cursor
+        self.cursor = start
+        return self._read_nbytes(header_end)
+
+    '''
+    Ocean Transaction sample
+    02000000    version
+    01          flag
+
+    01          # of vins
+    0000000000000000000000000000000000000000000000000000000000000000    vin prevhash
+    ffffffff    vin prev_idx
+    03  530101  script
+    ffffffff    sequence
+        - issuance example (if prev_idx & TxInputOcean.OUTPOINT_ISSUANCE_FLAG)
+        0000000000000000000000000000000000000000000000000000000000000000    nonce 32bytes
+        0000000000000000000000000000000000000000000000000000000000000000    entropy 32bytes
+        01  00038d7ea4c68000  amount (confidential value)
+        00                    inflation (confidential value)
+
+    02          # of vouts
+
+    01  8f9390e4c7b981e355aed3c5690e17c2e13bb263246a55d8039813cac670c2f1    asset (confidential asset)
+    01  000000000000de80    value (confidential value)
+    00                      nonce (confidential nonce)
+    01  51                  script
+
+    01  8f9390e4c7b981e355aed3c5690e17c2e13bb263246a55d8039813cac670c2f1
+    01  0000000000000000
+    00
+    26  6a24aa21a9ed2127440070600b5e8482e5df5815cc15b8262acf7533136c501f3cb4801faaf6
+
+    00000000 locktime
+
+    # for each vin - CTxInWitness
+    00  issuance amount range proof
+    00  inflation range proof
+    01  num of script witnesses
+    20 0000000000000000000000000000000000000000000000000000000000000000 script witness
+    00  num of pegin witnesses
+
+    # for each vout - CTxOutWitness
+    00  surjection proof
+    00  range proof
+    00  surjection proof
+    00  range proof
+    '''
+    def read_tx(self):
+        return self._read_tx_parts()[0]
+
+    def read_tx_and_hash(self):
+        tx, tx_hash, vsize = self._read_tx_parts()
+        return tx, tx_hash
+
+    def read_tx_and_vsize(self):
+        tx, tx_hash, vsize = self._read_tx_parts()
+        return tx, vsize
+
+    def _read_tx_parts(self):
+        '''Return a (deserialized TX, tx_hash, vsize) tuple.'''
+        start = self.cursor
+        version = self._read_le_int32()
+        orig_ser = self.binary[start:self.cursor]
+
+        flag = self._read_byte()    # for witness
+        orig_ser += b'\x00'     # for serialization hash flag is always 0
+
+        start = self.cursor
+        inputs = self._read_inputs()
+        outputs = self._read_outputs()
+        locktime = self._read_le_uint32()
+
+        orig_ser += self.binary[start:self.cursor]
+
+        start = self.cursor
+        witness_in = []
+        witness_out = []
+        if flag & 1:
+            witness_in = self._read_witness_in(len(inputs))
+            witness_out = self._read_witness_out(len(outputs))
+
+        base_size = len(orig_ser)
+        full_size = base_size + len(self.binary[start:self.cursor])
+        vsize = ((self.WITNESS_SCALE_FACTOR-1) * base_size + full_size + self.WITNESS_SCALE_FACTOR - 1) // self.WITNESS_SCALE_FACTOR
+
+        #print(double_sha256(orig_ser))
+        return TxOcean(version, flag, inputs, outputs, witness_in, witness_out,
+                        locktime), double_sha256(orig_ser), vsize
+
+    def _read_input(self):
+        '''Return a TxInputOcean object'''
+        prev_hash = self._read_nbytes(32)
+        prev_idx = self._read_le_uint32()
+        script = self._read_varbytes()
+        sequence = self._read_le_uint32()
+
+        issuance = None
+        if prev_idx != TxInputOcean.MINUS_1:
+            if prev_idx & TxInputOcean.OUTPOINT_ISSUANCE_FLAG:
+                issuance_nonce = self._read_nbytes(32)
+                issuance_entropy = self._read_nbytes(32)
+
+                amount = self._read_confidential_value()
+                inflation = self._read_confidential_value()
+
+                issuance = TxInputIssuanceOcean(
+                    issuance_nonce,
+                    issuance_entropy,
+                    amount,
+                    inflation
+                )
+                prev_idx &= TxInputOcean.OUTPOINT_INDEX_MASK
+
+        return TxInputOcean(
+            prev_hash,
+            prev_idx,
+            script,
+            sequence,
+            issuance
+        )
+
+    def _read_output(self):
+        '''Return a TxOutputOcean object'''
+        asset = self._read_confidential_asset()
+        value = self._read_confidential_value()
+        nonce = self._read_confidential_nonce()
+        script = self._read_varbytes()
+
+        return TxOutputOcean(
+            asset,
+            value,
+            nonce,
+            script
+        )
+
+    # CConfidentialValue size 9, prefixA 8, prefixB 9
+    def _read_confidential_value(self):
+        version = self._read_byte()
+        if version == 1 or version == 0xff:
+            return bytes([version]) + self._read_nbytes(TxOcean.CONFIDENTIAL_VALUE-1)
+        elif version == 8 or version == 9:
+            return bytes([version]) + self._read_nbytes(TxOcean.CONFIDENTIAL_COMMITMENT-1)
+        return bytes([version])
+
+    # CConfidentialAsset size 33, prefixA 10, prefixB 11
+    def _read_confidential_asset(self):
+        version = self._read_byte()
+        if version == 1 or version == 0xff:
+            return bytes([version]) + self._read_nbytes(TxOcean.CONFIDENTIAL_COMMITMENT-1)
+        elif version == 10 or version == 11:
+            return bytes([version]) + self._read_nbytes(TxOcean.CONFIDENTIAL_COMMITMENT-1)
+        return bytes([version])
+
+    # CConfidentialNonce size 33, prefixA 2, prefixB 3
+    def _read_confidential_nonce(self):
+        version = self._read_byte()
+        if version == 1 or version == 0xff:
+            return bytes([version]) + self._read_nbytes(TxOcean.CONFIDENTIAL_COMMITMENT-1)
+        elif version == 2 or version == 3:
+            return bytes([version]) + self._read_nbytes(TxOcean.CONFIDENTIAL_COMMITMENT-1)
+        return bytes([version])
+
+    def _read_witness_in(self, fields):
+        read_witness_in_field = self._read_witness_in_field
+        return [read_witness_in_field() for i in range(fields)]
+
+    def _read_witness_in_field(self):
+        read_varbytes = self._read_varbytes
+        issuance_range_proof = read_varbytes()
+        inflation_range_proof = read_varbytes()
+        script_witness = [read_varbytes() for i in range(self._read_varint())]
+        pegin_witness = [read_varbytes() for i in range(self._read_varint())]
+
+        return [issuance_range_proof, inflation_range_proof, script_witness, pegin_witness]
+
+    def _read_witness_out(self, fields):
+        read_witness_out_field = self._read_witness_out_field
+        return [read_witness_out_field() for i in range(fields)]
+
+    def _read_witness_out_field(self):
+        read_varbytes = self._read_varbytes
+        surjection_proof = read_varbytes()
+        range_proof = read_varbytes()
+
+        return [surjection_proof, range_proof]
 
 class DeserializerAuxPow(Deserializer):
     VERSION_AUXPOW = (1 << 8)
